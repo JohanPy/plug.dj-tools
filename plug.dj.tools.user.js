efc6854ec2430ad706ea6d8c5bef9eb7ea45c045
CopperFr <copper_france@yahoo.fr>
2019-04-23 23:06:24 +0200

    Update plug.dj.tools.user.js

--- plug.dj.tools.user.js
+++ plug.dj.tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         plug.dj.tools
 // @namespace    https://github.com/CopperFr/plug.dj-tools/
-// @version      1.3.1
+// @version      1.3.2
 // @description  Some tools for plug.dj
 // @author       CopperFr
 // @match        https://plug.dj/*
