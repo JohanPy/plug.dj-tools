c081b461ab407d7b68b28c8dac6977957d0c2fc6
CopperFr <copper_france@yahoo.fr>
2019-04-23 17:36:57 +0200

    Update plug.dj.tools.user.js

--- plug.dj.tools.user.js
+++ plug.dj.tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         plug.dj.tools
 // @namespace    https://github.com/CopperFr/plug.dj-tools/
-// @version      1.2.1
+// @version      1.3.1
 // @description  Some tools for plug.dj
 // @author       CopperFr
 // @match        https://plug.dj/*
@@ -9,16 +9,11 @@
 // ==/UserScript==
 
 (function() {
-let script = document.createElement('script');
-script.text = "if (typeof plugDJToolsExtensionUninstall == 'function') plugDJToolsExtensionUninstall();";
-(document.head || document.documentElement).appendChild(script);
-script.remove();
-
 script = document.createElement('script');
 script.src = 'https://copperfr.github.io/plug.dj-tools/tools.js';
 script.onload = function()
 {
   this.remove();
 };
-(document.head || document.documentElement).appendChild(script);;
+(document.head || document.documentElement).appendChild(script);
 })();
